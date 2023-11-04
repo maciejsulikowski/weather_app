85129dbfeab5540c81d4438ade82e3c6977f30ce
macieksulikowski <suleymaniana99@gmail.com>
2023-11-04 16:17:29 +0100

    Add button to intro_widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,30 +31,57 @@ class IntroWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color.fromARGB(255, 10, 58, 214),
-            Color.fromARGB(255, 22, 20, 129),
-          ],
-        )),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
+      child: SizedBox(
+        child: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(255, 10, 58, 214),
+              Color.fromARGB(255, 22, 20, 129),
+            ],
+          )),
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Text('WEATHER APP',
+                    style: GoogleFonts.aBeeZee(
+                      fontSize: 32,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: 250,
+                  height: 250,
+                  child: Lottie.asset('images/animation_1.json'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text('Check the weather from the place you want',
+                    style: GoogleFonts.aBeeZee(
+                      fontSize: 16,
+                      color: Colors.white,
+                    )),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  child: Text('Get Started',
+                      style: GoogleFonts.aBeeZee(
+                        fontSize: 16,
+                        color: Colors.white,
+                      )),
+                )
+              ],
             ),
-            Center(
-              child: Text('WEATHER APP',
-                  style: GoogleFonts.aBeeZee(
-                    fontSize: 32,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  )),
-            ),
-          ],
+          ),
         ),
       ),
     ));
