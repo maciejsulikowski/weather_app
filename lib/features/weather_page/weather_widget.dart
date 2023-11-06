d40271bf7b53530809570258c1b38bd4626f8e88
macieksulikowski <suleymaniana99@gmail.com>
2023-11-06 23:00:10 +0100

    Add RainfallWidget and new possibilities

--- lib/features/weather_page/weather_widget.dart
+++ lib/features/weather_page/weather_widget.dart
@@ -164,6 +164,44 @@ class WeatherWidget extends StatelessWidget {
                   const SizedBox(
                     height: 10,
                   ),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: const LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Color.fromARGB(255, 5, 36, 136),
+                                Color.fromARGB(255, 47, 46, 128),
+                              ],
+                            )),
+                        child: Column(
+                          children: [
+                            Text('10 AM',
+                                style: GoogleFonts.aBeeZee(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            SizedBox(
+                              height: 70,
+                              child: Image.asset(
+                                'images/water.png',
+                              ),
+                            ),
+                            Text('27°C',
+                                style: GoogleFonts.aBeeZee(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
                 ],
               ),
             ),
