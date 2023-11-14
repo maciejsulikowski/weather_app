c5e42b070540c78a76688a6f3ec945818243614f
macieksulikowski <suleymaniana99@gmail.com>
2023-11-14 11:29:08 +0100

    Create enums file

--- lib/app/features/intro_page/intro_widget.dart
+++ lib/app/features/intro_page/intro_widget.dart
@@ -27,63 +27,60 @@ class _IntroWidgetState extends State<IntroWidget> {
             Color.fromARGB(255, 22, 20, 129),
           ],
         )),
-        child: Hero(
-          tag: 'weather_app',
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                Text('WEATHER APP',
-                    style: GoogleFonts.aBeeZee(
-                      fontSize: 40,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    )),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  width: double.infinity,
-                  height: 250,
-                  child: Lottie.asset('images/animation_1.json'),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Text('Check the weather ⛅',
-                    style: GoogleFonts.aBeeZee(
-                      fontSize: 32,
-                      color: Colors.white,
-                    )),
-                const SizedBox(
-                  height: 30,
-                ),
-                Text('Whenever you want ⭐',
-                    style: GoogleFonts.aBeeZee(
-                      fontSize: 24,
-                      color: Colors.white,
-                    )),
-                const SizedBox(
-                  height: 30,
-                ),
-                Text('Wherever you want 🌈',
-                    style: GoogleFonts.aBeeZee(
-                      fontSize: 24,
-                      color: Colors.white,
-                    )),
-                const SizedBox(
-                  height: 30,
-                ),
-                const IntroButton(),
-                // ElevatedButton(
-                //   child: Text('Contact us'),
-                //   onPressed: () => returnprint('Contact us'),
-                // )
-              ],
-            ),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Text('WEATHER APP',
+                  style: GoogleFonts.aBeeZee(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                width: double.infinity,
+                height: 250,
+                child: Lottie.asset('images/animation_1.json'),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Text('Check the weather ⛅',
+                  style: GoogleFonts.aBeeZee(
+                    fontSize: 32,
+                    color: Colors.white,
+                  )),
+              const SizedBox(
+                height: 30,
+              ),
+              Text('Whenever you want ⭐',
+                  style: GoogleFonts.aBeeZee(
+                    fontSize: 24,
+                    color: Colors.white,
+                  )),
+              const SizedBox(
+                height: 30,
+              ),
+              Text('Wherever you want 🌈',
+                  style: GoogleFonts.aBeeZee(
+                    fontSize: 24,
+                    color: Colors.white,
+                  )),
+              const SizedBox(
+                height: 30,
+              ),
+              const IntroButton(),
+              // ElevatedButton(
+              //   child: Text('Contact us'),
+              //   onPressed: () => returnprint('Contact us'),
+              // )
+            ],
           ),
         ),
       ),
