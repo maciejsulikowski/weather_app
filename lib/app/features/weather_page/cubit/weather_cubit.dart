d1dad460b2206ce198b52a9bab3cbd0efad07adc
macieksulikowski <suleymaniana99@gmail.com>
2023-11-28 12:24:01 +0100

    Add getForecast event

--- lib/app/features/weather_page/cubit/weather_cubit.dart
+++ lib/app/features/weather_page/cubit/weather_cubit.dart
@@ -2,10 +2,27 @@ import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:weather_app/app/core/enums.dart';
 import 'package:weather_app/app/domain/models/weather_model.dart';
+import 'package:weather_app/app/domain/repositories/search_repository.dart';
 
 part 'weather_state.dart';
 
-
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit() : super(WeatherState());
+  WeatherCubit(this.searchRepository) : super(const WeatherState());
+
+  final SearchRepository searchRepository;
+
+  Future<void> getForecast(String key) async {
+    emit(const WeatherState(status: Status.loading));
+
+    final weather = await searchRepository.getForecastModel(key);
+
+    try {
+      emit(WeatherState(
+        dailyForecast: weather,
+        status: Status.success,
+      ));
+    } catch (error) {
+      emit(WeatherState(status: Status.error, errorMessage: error.toString()));
+    }
+  }
 }
--- lib/app/features/weather_page/cubit/weather_state.dart
+++ lib/app/features/weather_page/cubit/weather_state.dart
@@ -3,12 +3,12 @@ part of 'weather_cubit.dart';
 @immutable
 class WeatherState {
   const WeatherState({
-    this.dailyForecast = const [],
+    this.dailyForecast,
     this.status = Status.initial,
     this.errorMessage,
   });
 
-  final List<WeatherModel> dailyForecast;
+  final WeatherModel? dailyForecast;
   final Status status;
   final String? errorMessage;
 }
